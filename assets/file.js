f9435cc055b943892a65e29a01918936b72a66d3
ndongogueye <bayeg2003@gmail.com>
2023-07-17 14:23:03 +0200

    #1

---
+++ assets/file.js
@@ -0,0 +1,20 @@
+// Dati forniti
+let membriTeam = [
+    {
+      nome: "Mario Rossi",
+      ruolo: "Sviluppatore",
+      foto: "https://example.com/foto1.jpg"
+    },
+    {
+      nome: "Luca Verdi",
+      ruolo: "Progettista",
+      foto: "https://example.com/foto2.jpg"
+    },
+    {
+      nome: "Anna Bianchi",
+      ruolo: "Tester",
+      foto: "https://example.com/foto3.jpg"
+    }
+  ];
+  
+ 
